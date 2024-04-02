211e7a522c07138a089a6edcdfaef037338863d3
juanm <jmloperas@eafit.edu.co>
2024-04-01 23:17:57 -0500

    Finish Project

---
+++ Main.py
@@ -0,0 +1,84 @@
+import time
+from Modulo import Student, Car, PriorityQueue, RegularQueue, clear
+
+priorityqueue = PriorityQueue()
+regularqueue = RegularQueue()
+creationqueue = []
+arrivalqueue = []
+
+clear()
+leave = False
+while not leave:
+    print("--------------------- EAFLYPASS Parking Lot ---------------------")
+    print("1. Add student to the system")
+    print("2. Add car to the queue")
+    print("3. Park cars")
+    print("4. Leave")
+    decision = input()
+    clear()
+    match decision:
+        case "1":
+            invalid = False
+            name = input("Name: ")
+            student_id = int(input("Student ID: "))
+            honor = input("Is the student enrolled with honors? ( Y / N ): ")
+            plate = input("What is the Vehicle Registration Plate of the student's car?: ")
+            eco = input("Is the student's car an ecologic car? ( Y / N): ")
+            if honor.lower() == "y" or eco.lower() == "y":
+                category = "Priority"
+            elif honor.lower() == "n" and eco.lower() == "n":
+                category = "Regular"
+            else:
+                print("You must answer either Y or N to the questions that ask you to do so")
+                invalid = True
+            if not invalid:
+                student = Student(name, student_id, category)
+                car = Car(student, plate)
+                creationqueue.append(car)
+                print("Student Added Correctly")
+            time.sleep(2)
+            clear()
+        case "2":
+            i = 1
+            for x in creationqueue:
+                if x not in arrivalqueue:
+                    print(str(i) + ". " + x.owner.name)
+                    print("Category: " + x.owner.category)
+                    print()
+                i += 1
+            index = int(input("Who is gonna join the queue?: "))
+            if 0 <= index - 1 < len(creationqueue):
+                if creationqueue[index - 1] not in arrivalqueue:
+                    arrivalqueue.append(creationqueue[index - 1])
+                    print("Car added successfully to the queue")
+                else:
+                    print("This car is already in the queue")
+            else:
+                print("Invalid index")
+            time.sleep(2)
+            clear()
+        case "3":
+            for x in arrivalqueue:
+                if x.owner.category == "Priority":
+                    priorityqueue.addCar(x)
+                elif x.owner.category == "Regular":
+                    regularqueue.addCar(x)
+            priorityqueue.parkCars()
+            regularqueue.parkCars()
+            arrivalqueue.clear()
+            print("All cars have been parked successfully, the queue is now empty")
+            time.sleep(4)
+            clear()
+        case "4":
+            print("Leaving...")
+            quit()
+        case _:
+            print("Invalid Input")
+
+
+
+
+
+
+
+
---
+++ Modulo.py
@@ -0,0 +1,162 @@
+import time
+
+def clear():
+    for x in range(50):
+        print()
+
+class Student:
+    def __init__(self, name: str, student_id: int, category: str):
+        self.name = name
+        self.college_id = student_id
+        self.category = category
+        self.parking_credits = 5
+
+    def useCredit(self):
+        self.parking_credits -= 1
+
+    def addCredit(self, answer):
+        if answer.lower() == "y":
+            valid = False
+            while not valid:
+                clear()
+                print("How many credits do you want to add?")
+                credit = int(input())
+                if (self.parking_credits + credit) >= 5:
+                    self.parking_credits += credit
+                    print("Credits added successfully")
+                    valid = True
+                else:
+                    print("Your total amount of credits has to be equal or higher than 5 if you want to add credits")
+                    time.sleep(2)
+            time.sleep(2)
+            clear()
+            return True
+        elif answer.lower() == "n":
+            print("Alright, just remember you need to have credits to park here")
+            time.sleep(2)
+            clear()
+            return True
+        else:
+            print("Invalid Answer")
+            print(" ")
+            time.sleep(2)
+            clear()
+            return False
+
+
+class Car:
+    def __init__(self, owner: Student, license_plate: str):
+        self.owner = owner
+        self.license_plate = license_plate
+        self.next = None
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def addCar(self, car: Car):
+        car.next = None
+        if not (self.front and self.rear):
+            self.front = car
+            self.rear = car
+        else:
+            self.rear.next = car
+            self.rear = car
+
+    def parkCars(self):
+        current = self.front
+        if not current:
+            print("There is no cars in the priority queue")
+        while current:
+            if current.owner.parking_credits > 1:
+                current.owner.useCredit()
+                print(current.owner.name + " welcome to the EAFLYPASS parking lot, priority user")
+                print("Vehicle Registration Plate: " + current.license_plate)
+                print("Remaining Credits: " + str(current.owner.parking_credits))
+            elif current.owner.parking_credits == 1:
+                current.owner.useCredit()
+                print("************** ALERT **************")
+                valid = False
+                while not valid:
+                    print(current.owner.name + ", you have wasted your last credit. "
+                                               "Do you want to add more credits? ( Y / N )")
+                    answer = input()
+                    valid = current.owner.addCredit(answer)
+                print(current.owner.name + " welcome to the EAFLYPASS parking lot, priority user")
+                print("Vehicle Registration Plate: " + current.license_plate)
+                print("Remaining Credits: " + str(current.owner.parking_credits))
+            else:
+                valid = False
+                while not valid:
+                    print(current.owner.name + ", you don't have enough credits to park here. "
+                                               "Do you want to add more credits? ( Y / N )")
+                    answer = input()
+                    valid = current.owner.addCredit(answer)
+                if current.owner.parking_credits >= 1:
+                    print(current.owner.name + " welcome to the EAFLYPASS parking lot, priority user")
+                    print("Vehicle Registration Plate: " + current.license_plate)
+                    print("Remaining Credits: " + str(current.owner.parking_credits))
+            time.sleep(2)
+            clear()
+            current = current.next
+        self.front = None
+        self.rear = None
+
+
+class RegularQueue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def addCar(self, car: Car):
+        car.next = None
+        if not (self.front and self.rear):
+            self.front = car
+            self.rear = car
+        else:
+            self.rear.next = car
+            self.rear = car
+
+    def parkCars(self):
+        current = self.front
+        if not current:
+            print("There are no cars in the regular queue")
+        while current:
+            if current.owner.parking_credits > 1:
+                current.owner.useCredit()
+                print(current.owner.name + " welcome to the EAFLYPASS parking lot")
+                print("Vehicle Registration Plate: " + current.license_plate)
+                print("Remaining Credits: " + str(current.owner.parking_credits))
+            elif current.owner.parking_credits == 1:
+                current.owner.useCredit()
+                print("************** ALERT **************")
+                valid = False
+                while not valid:
+                    print(current.owner.name + ", you have wasted your last credit. "
+                                               "Do you want to add more credits? ( Y / N )")
+                    answer = input()
+                    valid = current.owner.addCredit(answer)
+                print(current.owner.name + " welcome to the EAFLYPASS parking lot")
+                print("Vehicle Registration Plate: " + current.license_plate)
+                print("Remaining Credits: " + str(current.owner.parking_credits))
+            else:
+                valid = False
+                while not valid:
+                    print(current.owner.name + ", you don't have enough credits to park here. "
+                                               "Do you want to add more credits? ( Y / N )")
+                    answer = input()
+                    valid = current.owner.addCredit(answer)
+                if current.owner.parking_credits >= 1:
+                    current.owner.useCredit()
+                    print(current.owner.name + " welcome to the EAFLYPASS parking lot")
+                    print("Vehicle Registration Plate: " + current.license_plate)
+                    print("Remaining Credits: " + str(current.owner.parking_credits))
+                elif current.owner.parking_credits == 0:
+                    print(current.owner.name + " could not park here")
+            time.sleep(2)
+            clear()
+            current = current.next
+        self.front = None
+        self.rear = None
